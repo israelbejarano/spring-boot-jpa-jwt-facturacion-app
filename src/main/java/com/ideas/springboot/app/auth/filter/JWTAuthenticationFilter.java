d953f35e99ce5682ec8659d81795ad06e4511e17
israel <israel.bejarano.a@gmail.com>
2019-08-08 20:28:52 +0200

    add más datos al jwt token

--- src/main/java/com/ideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/ideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,7 +1,8 @@
 package com.ideas.springboot.app.auth.filter;
 
 import java.io.IOException;
-
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +15,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.ideas.springboot.app.models.entity.Usuario;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -73,9 +76,15 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 			Authentication authResult) throws IOException, ServletException {
 
 		String username = ((User) authResult.getPrincipal()).getUsername();
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
 		String token = Jwts.builder()
+				.setClaims(claims)
 				.setSubject(username)
 				.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.123456".getBytes())
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 3600000*4L))
 				.compact();
 		
 		response.addHeader("Authorization", "Bearer " + token);

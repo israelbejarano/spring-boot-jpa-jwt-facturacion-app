703fdedebf9e9cb880c9ec2f50073e9720033a45
israel <israel.bejarano.a@gmail.com>
2019-08-08 21:23:59 +0200

    add javadoc

--- src/main/java/com/ideas/springboot/app/auth/SimpleGrantedAuthoritiesMixin.java
+++ src/main/java/com/ideas/springboot/app/auth/SimpleGrantedAuthoritiesMixin.java
@@ -3,8 +3,17 @@ package com.ideas.springboot.app.auth;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The Class SimpleGrantedAuthoritiesMixin. Clase para poder parsear la petición a json
+ * @author Israel Bejarano
+ */
 public abstract class SimpleGrantedAuthoritiesMixin {
 	
+	/**
+	 * Instantiates a new simple granted authorities mixin.
+	 *
+	 * @param role the role
+	 */
 	@JsonCreator
 	public SimpleGrantedAuthoritiesMixin(@JsonProperty("authority") String role) {
 		
